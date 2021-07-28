b67a6b3ce1f356da4a3d7aa3c4b5390d70e3bc48
Volkov Alexander <volkov.525@gmail.com>
2021-07-28 16:46:55 +0300

    add font size slider

--- src/components/RDrawer.vue
+++ src/components/RDrawer.vue
@@ -2,6 +2,12 @@
 q-drawer(v-model="show" side="right" bordered )
 	Color(:swatches="maincolor")
 	q-item
+		q-item-section(avatar)
+			q-icon( name="mdi-format-text")
+		q-item-section
+			q-slider(v-model="size" :min="11" :max="18" label color="primary" @change="setSize")
+	q-separator
+	q-item
 		q-toggle( v-model="color.toolbar" label="Контрастный тулбар" color="primary")
 	q-item
 		q-toggle( v-model="color.reveal" label="Авто-скрытие тулбара" color="primary")
@@ -28,6 +34,7 @@ export default {
 		const panel = ref(false)
 		const $q = useQuasar()
 		const color = useColor()
+		const size = ref(14)
 
 		const maincolor = reactive([
 			{ id: 'one', color: 'one', icon: true },
@@ -37,6 +44,12 @@ export default {
 		])
 		const link = reactive([])
 
+		const setSize = () => {
+			let parameter = '--my-body-font-size'
+			let newsize = size.value + 'px'
+			document.body.style.setProperty(parameter, newsize)
+		}
+
 		watch(
 			() => dark.value,
 			() => {
@@ -45,6 +58,7 @@ export default {
 		)
 
 		return {
+			setSize,
 			dark,
 			toolbar,
 			panel,
@@ -52,6 +66,7 @@ export default {
 			props,
 			maincolor,
 			link,
+			size,
 		}
 	},
 }
