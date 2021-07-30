451254a967d74a81969b0cb1ba6ef09022285914
Volkov Alexander <volkov.525@gmail.com>
2021-07-30 14:25:09 +0300

    add lib component

--- src/components/Lib.vue
+++ src/components/Lib.vue
@@ -1,20 +1,5 @@
 <template lang="pug">
-q-drawer(v-model="show" side="left" :mini="mini" :width="width" bordered :class="{ fill : color.panel }")
-	q-list(v-if="!lib")
-		q-item(show-if-above clickable v-ripple :to="page.url" v-for="page in pages" :key="page.id")
-			q-item-section(avatar)
-				q-icon(:name="page.icon")
-			q-item-section {{ page.title }}
-
-		q-item(clickable v-ripple).bottom
-			q-item-section(avatar)
-				q-icon(name="mdi-puzzle-outline")
-			q-item-section Библиотека
-	Lib(v-else)
-	
-
-	q-btn(round flat dense :icon="minitoogle" @click="mini = !mini").mini
-	
+h3 hello
 
 </template>
 
@@ -22,28 +7,12 @@ q-drawer(v-model="show" side="left" :mini="mini" :width="width" bordered :class=
 import { ref, computed } from 'vue'
 import { maincolor } from '@/utils/utils'
 import { useColor } from '@/stores/colors'
-import Lib from '@/components/Lib.vue'
 
 export default {
 	props: ['show'],
-	components: {Lib},
 	setup() {
 		const color = useColor()
-		const pages = [
-			{ id: 1, title: 'Главная', icon: 'mdi-home-roof', url: '/' },
-			{ id: 2, title: 'Документы', icon: 'mdi-text-box-outline', url: '/docs' },
-			{ id: 3, title: 'Задания', icon: 'mdi-check', url: '/tasks' },
-			{
-				id: 4,
-				title: 'Папки',
-				icon: 'mdi-folder-outline',
-				url: '/folders',
-			},
-			// { id: 5, title: 'Hello', icon: 'mdi-folder-outline', url: '/hello' },
-		]
 		const mini = ref(false)
-		const width = 256
-		const lib = true
 
 		const minitoogle = computed(() => {
 			return mini.value ? 'mdi-forwardburger' : 'mdi-backburger'
@@ -51,11 +20,8 @@ export default {
 
 		return {
 			color,
-			width,
-			lib,
 			maincolor,
 			mini,
-			pages,
 			minitoogle,
 		}
 	},
