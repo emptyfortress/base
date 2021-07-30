0008f2c23a0babb14a4a9ac33f99886357ace284
Volkov Alexander <volkov.525@gmail.com>
2021-07-30 13:05:04 +0300

    all color setup working

--- src/components/Color.vue
+++ src/components/Color.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-q-item-label(header) Основной цвет:
+q-item-label(header) Выберите основной цвет:
 q-item
 	.q-mx-md
 		q-btn(round unelevated :color="swatch.color" v-for="swatch in swatches" :key="swatch.id" :id="swatch.id" @click="setColor(swatch.id, swatch.color)").swatch
@@ -18,7 +18,6 @@ q-separator
 
 <script>
 import { ref } from 'vue'
-import { colors } from 'quasar'
 
 export default {
 	props: {
@@ -57,7 +56,50 @@ export default {
 				g = '0x' + h[3] + h[4]
 				b = '0x' + h[5] + h[6]
 			}
+			rgbToHSL(r, g, b)
 			return 'rgb(' + +r + ',' + +g + ',' + +b + ')'
+		}
+
+		function rgbToHSL(r, g, b) {
+			// Then to HSL
+			r /= 255
+			g /= 255
+			b /= 255
+			let cmin = Math.min(r, g, b),
+				cmax = Math.max(r, g, b),
+				delta = cmax - cmin,
+				h = 0,
+				s = 0,
+				l = 0
+
+			if (delta == 0) h = 0
+			else if (cmax == r) h = ((g - b) / delta) % 6
+			else if (cmax == g) h = (b - r) / delta + 2
+			else h = (r - g) / delta + 4
+
+			h = Math.round(h * 60)
+
+			if (h < 0) h += 360
+
+			l = (cmax + cmin) / 2
+			s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1))
+			s = +(s * 100).toFixed(1)
+			l = +(l * 100).toFixed(1)
+
+			// set lighten variables
+			let lighten1 = 'hsl(' + h + ',' + s + '%,' + '75%)'
+			let lighten2 = 'hsl(' + h + ',' + s + '%,' + '84%)'
+			let lighten3 = 'hsl(' + h + ',' + s + '%,' + '91%)'
+			document.body.style.setProperty('--q-primary-lighten-1', lighten1)
+			document.body.style.setProperty('--q-primary-lighten-2', lighten2)
+			document.body.style.setProperty('--q-primary-lighten-3', lighten3)
+			// set darken variables
+			let darken1 = 'hsl(' + h + ',' + s + '%,' + '45%)'
+			let darken2 = 'hsl(' + h + ',' + s + '%,' + '35%)'
+			let darken3 = 'hsl(' + h + ',' + s + '%,' + '25%)'
+			document.body.style.setProperty('--q-primary-darken-1', darken1)
+			document.body.style.setProperty('--q-primary-darken-2', darken2)
+			document.body.style.setProperty('--q-primary-darken-3', darken3)
 		}
 
 		const setColor = (id, e) => {
@@ -69,10 +111,13 @@ export default {
 			let myBt = document.getElementById(id)
 			let currentColor = window.getComputedStyle(myBt).backgroundColor
 			patch.value = RGBToHex(currentColor)
+			hexToRGB(patch.value)
 		}
+
 		const changeColor = () => {
 			let currentColorName = props.swatches.find((item) => item.icon).color
 			let newColor = document.getElementById('colorPatch').value
+			console.log(newColor)
 			let colorRgb = hexToRGB(newColor)
 			let combineName = '--' + currentColorName
 			document.body.style.setProperty(combineName, colorRgb)
