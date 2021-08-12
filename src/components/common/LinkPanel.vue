b68585c53a7139a5a12d5573e828d141ac0fcb59
Volkov Alexander <volkov.525@gmail.com>
2021-08-12 12:08:54 +0300

    add icons to link panel

--- src/components/common/LinkPanel.vue
+++ src/components/common/LinkPanel.vue
@@ -1,8 +1,14 @@
 <template lang="pug">
 .relative-position
-	q-expansion-item(v-model='panels.link' switch-toggle-side label="Ссылки")
+	q-expansion-item(v-model='panels.link' switch-toggle-side label='Ссылки')
 		q-card
 			q-card-section laksjd
+
+	.actionBt.gr
+		q-btn(round flat dense )
+			SvgIcon(name="plus")
+		q-btn(round flat dense )
+			SvgIcon(name="sliders-vertical")
 </template>
 
 <script>
@@ -21,4 +27,18 @@ export default {
 
 <style scoped lang="scss">
 @import '@/assets/styles/theme.scss';
+.actionBt {
+	position: absolute;
+	top: 7px;
+	right: 16px;
+	body.body--dark & {
+		color: var(--dark-text-color);
+	}
+	&.gr svg {
+		opacity: 0.7;
+		&:hover {
+			opacity: 1;
+		}
+	}
+}
 </style>
