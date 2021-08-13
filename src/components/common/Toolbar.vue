b6975fc28bdbe90583eebeaeedfb27232d5b6069
Volkov Alexander <volkov.525@gmail.com>
2021-08-13 10:29:21 +0300

    add switch grid - lenta

--- src/components/common/Toolbar.vue
+++ src/components/common/Toolbar.vue
@@ -4,21 +4,19 @@
 		q-btn(flat round dense @click="grid.switchSidebar")
 			q-icon(name="mdi-backburger" v-if="grid.sidebar")
 			q-icon(name="mdi-forwardburger" v-else)
+		.total Всего:
+			span 43
+		q-btn(unelevated size="12px") Показать все
+	.center()
+		q-btn-group(unelevated).group
+			q-btn(flat round dense icon="mdi-table" size="12px")
+				q-tooltip(:delay="600" anchor="top middle" self="center middle") Грид
+			q-btn(color="primary" dense icon="mdi-format-list-bulleted" size="12px")
+				q-tooltip(:delay="600" anchor="top middle" self="center middle") Лента
 	.right
-		q-btn(flat round dense )
-			SvgIcon(name="sort-variant")
-		q-btn(flat round dense )
-			SvgIcon(name="readAll")
-		q-btn(flat round dense )
-			SvgIcon(name="refresh")
-		q-btn(flat round dense )
-			SvgIcon(name="xls-export")
-		q-btn(flat round dense )
-			SvgIcon(name="sliders-reload")
-		q-btn(flat round dense )
-			SvgIcon(name="sliders-vertical")
-		q-btn(flat round dense )
-			SvgIcon(name="pen")
+		q-btn(flat round dense v-for="button in buttons")
+			q-tooltip(:delay="600" anchor="top middle" self="center middle") {{ button.tooltip}}
+			SvgIcon(:name="button.icon")
 </template>
 
 <script>
@@ -33,11 +31,20 @@ export default {
 	setup() {
 		const grid = useGrid()
 
+		const buttons = [
+			{ id: 0, icon: 'sort-variant', tooltip: 'Сортировка', action: '' },
+			{ id: 1, icon: 'readAll', tooltip: 'Прочитать все', action: '' },
+			{ id: 2, icon: 'refresh', tooltip: 'Обновить', action: '' },
+			{ id: 3, icon: 'xls-export', tooltip: 'Экспорт', action: '' },
+			{ id: 4, icon: 'sliders-reload', tooltip: 'Сброс настроек', action: '' },
+			{ id: 5, icon: 'sliders-vertical', tooltip: 'Настройки', action: '' },
+		]
+
 		return {
 			grid,
+			buttons,
 		}
 	},
-
 }
 </script>
 
@@ -45,14 +52,25 @@ export default {
 @import '@/assets/styles/theme.scss';
 
 .toolbar {
-	padding: 8px;
 	border-top: 1px solid var(--my-border-color);
 	border-bottom: 1px solid var(--my-border-color);
 	/* background: #fff; */
-	height: 40px;
+	height: 42px;
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
-
+}
+.total {
+	/* font-size: 1.0rem; */
+	display: inline-block;
+	line-height: 42px;
+	margin: 0 1rem;
+	span {
+		font-weight: 600;
+		margin-left: 0.5rem;
+	}
+}
+.group {
+	border: 1px solid var(--q-primary);
 }
 </style>
--- src/stores/grid.js
+++ src/stores/grid.js
@@ -4,11 +4,16 @@ export const useGrid = defineStore({
 	id: 'grid',
 	state: () => ({
 		sidebar: true,
+		view: 'grid',
 	}),
 	getters: {},
 	actions: {
 		switchSidebar() {
 			this.sidebar = !this.sidebar
+		},
+		switchView() {
+			this.view === 'grid' ? this.view = 'lenta' : this.view = 'grid'
+			
 		}
 	},
 })
