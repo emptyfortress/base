5c3832d4b747a6f2c4423367ccd356deb57cc295
Volkov Alexander <volkov.525@gmail.com>
2021-08-20 14:02:53 +0300

    add text-highlighter

--- src/components/common/Filter.vue
+++ src/components/common/Filter.vue
@@ -1,28 +1,52 @@
 <template lang="pug">
 q-card.quick.shadow-3(v-show="filterByIndex === col" @click.stop )
 	q-card-section.q-pb-none.q-pt-xs
-		q-input(dense square input-class="filter-input")
-	q-list().list
-		q-item(v-for="(dat, index) in data" :key="index" clickable v-ripple)
+		q-input(dense square
+			input-class="filter-input"
+			v-model="filter"
+			autofocus
+			clearable
+			)
+	q-list(v-if="filteredItems.length")
+		q-item(v-for="(dat, index) in filteredItems" :key="index" tag="label" v-ripple ).q-pa-none
 			q-item-section(side top)
 				q-checkbox(v-model="checked" :val="dat")
 			q-item-section
-				q-item-label {{ dat }}
+				q-item-label
+					WordHighlighter(:query="filter") {{ dat }}
+	.empty(v-else)
+		q-icon(name="mdi-circle-off-outline")
+		span Нет совпадений
 	q-separator
-	pre {{ checked }}
 	q-card-actions(align="between")
 		q-btn(flat round size="12px" icon="mdi-trash-can-outline" color="negative" @click="$emit('close')")
 		q-btn(flat size="12px" color="primary" @click="$emit('close')") Применить
 </template>
 
 <script>
+import WordHighlighter from 'vue-word-highlighter'
+
 export default {
 	props: ['filterByIndex', 'col', 'data' ],
+	components: {
+		WordHighlighter,
+	},
 	data() {
 		return {
 			checked: [],
+			filter: '',
 		}
 	},
+	computed: {
+		filteredItems() {
+			return this.data.filter( row => {
+				if (this.filter) {
+					return row.toLowerCase().includes(this.filter.toLowerCase())
+				}
+				return this.data
+			})
+		}
+	}
 }
 </script>
 
@@ -44,6 +68,25 @@ export default {
 	.q-list {
 		max-height: 300px;
 		overflow: auto;
+		font-size: 0.8rem;
+		font-weight: 400;
+		.q-item {
+			min-height: 36px;
+			padding: 5px 10px;
+		}
+	}
+}
+.empty {
+	padding: .7rem 1rem;
+	color: grey;
+	font-size: 0.8rem;
+	min-height: 70px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	.q-icon {
+		font-size: 1.0rem;
+		margin-right: .5rem;
 	}
 }
 </style>
