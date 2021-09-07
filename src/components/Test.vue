d1ac84a31c50510f57adf083a9e73b310008f9e5
Volkov Alexander <volkov.525@gmail.com>
2021-09-07 10:16:34 +0300

    add file

---
+++ src/components/Test.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+h1 Hello
+add some text
+and another text
+</template>
+
+<script>
+export default {
+
+	lkajsldkjalkj
+	some 
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/styles/theme.scss';
+other
+</style>
