2a0a489acef01bcf0360288dde9eb4747886d313
Volkov Alexander <volkov.525@gmail.com>
2022-11-25 19:06:06 +0300

    fix input

--- src/pages/Cabinet.vue
+++ src/pages/Cabinet.vue
@@ -1,8 +1,32 @@
 <template lang="pug">
 q-page(padding)
-	h2 Hello!
+	.row.justify-between.items-center
+		.zag
+			q-icon(name="mdi-card-account-details-outline").q-mr-md
+			|Личный кабинет
+		q-input(v-model="search" dense clearable autofocus ).search
+			template(v-slot:prepend)
+				q-icon(name="mdi-magnify")
+
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
 
-<style scoped lang="scss"></style>
+const search = ref()
+</script>
+
+<style scoped lang="scss">
+.search {
+	width: 350px;
+	font-size: 1.1rem;
+	margin-bottom: 1rem;
+	:deep .q-field--prepend,
+	:deep .q-field--append {
+		transform: translateY(5px);
+	}
+}
+:deep.q-field--dense .q-field__marginal {
+	height: inherit !important;
+}
+</style>
