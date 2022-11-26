4da44422000135e6348d6e94ba7dbeb9a7ff45c6
Volkov Alexander <volkov.525@gmail.com>
2022-11-26 13:38:13 +0300

    wip main layout

---
+++ src/components/icons/DocIcon.vue
@@ -0,0 +1,34 @@
+<template>
+	<svg
+		width="35"
+		height="42"
+		viewBox="0 0 35 42"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			d="M6.5 36C7.88071 36 9 34.8807 9 33.5C9 32.1193 7.88071 31 6.5 31C5.11929 31 4 32.1193 4 33.5C4 34.8807 5.11929 36 6.5 36Z"
+			fill="currentColor"
+		/>
+		<path d="M4 5H6V17H4V5Z" fill="currentColor" />
+		<path d="M9 5H7V17H9V5Z" fill="currentColor" />
+		<path
+			d="M17.5 36C18.8807 36 20 34.8807 20 33.5C20 32.1193 18.8807 31 17.5 31C16.1193 31 15 32.1193 15 33.5C15 34.8807 16.1193 36 17.5 36Z"
+			fill="currentColor"
+		/>
+		<path d="M15 5H17V17H15V5Z" fill="currentColor" />
+		<path d="M20 5H18V17H20V5Z" fill="currentColor" />
+		<path
+			d="M28.5 36C29.8807 36 31 34.8807 31 33.5C31 32.1193 29.8807 31 28.5 31C27.1193 31 26 32.1193 26 33.5C26 34.8807 27.1193 36 28.5 36Z"
+			fill="currentColor"
+		/>
+		<path d="M26 5H28V17H26V5Z" fill="currentColor" />
+		<path d="M31 5H29V17H31V5Z" fill="currentColor" />
+		<path
+			fill-rule="evenodd"
+			clip-rule="evenodd"
+			d="M0 0V42H35V0H0ZM11 1.6V40.4H1.6V1.6H11ZM22 1.6V40.4H13V1.6H22ZM33.4 40.4H24V1.6H33.4V40.4Z"
+			fill="currentColor"
+		/>
+	</svg>
+</template>
--- src/pages/Cabinet.vue
+++ src/pages/Cabinet.vue
@@ -1,32 +1,161 @@
 <template lang="pug">
 q-page(padding)
 	.row.justify-between.items-center
-		.zag
-			q-icon(name="mdi-card-account-details-outline").q-mr-md
-			|Личный кабинет
-		q-input(v-model="search" dense clearable autofocus ).search
-			template(v-slot:prepend)
-				q-icon(name="mdi-magnify")
+		.flex.items-center
+			q-avatar(size="80px" color="deep-purple-3").q-mr-md
+				img(src="@/assets/img/users/user0.svg")
+			.zag Орлов Петр Иванович
+		.overline Таб.№ 0002l34-m&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Водитель
+	.mygrid
+		.block
+			.hd
+				|Зарплата
+				span октябрь 2022
+					q-menu
+						q-list
+							q-item(clickable v-close-popup)
+								q-item-section laskj
+			component(:is="SvgIcon" name="money" @click.stop="toggleMoney").mon
+			.tb(:class="{ blur : money}")
+				.text-bold Начислено:
+				.text-bold.text-right 75,000.45
+				q-separator
+				div Оплата по окладу:
+				.text-right 50,000.00
+				div Отпуск основной:
+				.text-right 20,000.00
+				.text-bold Удержано:
+				.text-bold.text-right 12,129.15
+				div НДФЛ:
+				.text-right 12,129.15
+				q-separator
+				.text-bold Наличные
+				.text-bold.text-right 63,452.34
+
+		.block
+			.hd Отпуск
+				q-badge(align="top") 1
+			component(:is="SvgIcon" name="beach" style="font-size:1.5rem; top:4px; right: 2px").mon
+			.q-mt-md 😀 Очередной отпуск через 23 дня.
+			//- q-btn(flat color="primary" dense) График отпусков
+			//- q-btn(flat color="primary" dense) Заявление на отпуск
+		.block
+			.hd Больничные листы
+			component(:is="SvgIcon" name="crest" style="font-size:1.4rem; top:9px; right:2px").mon
+		.block
+			.hd Командировки
+			component(:is="SvgIcon" name="plane" style="font-size:2.7rem; top: -10px").mon
+			.q-mt-sm
+				q-btn(dense flat color="primary" size="12px").q-mr-md Оформить командировку
+				q-btn(dense flat color="primary" size="12px") Сдать отчет
+		.block
+			.hd Справки
+			component(:is="SvgIcon" name="zayav" style="font-size:1.3rem;top: 8px" ).mon
+			.q-mt-sm
+				q-btn(dense flat color="primary" size="12px").q-mr-md Справка о доходах
+				q-btn(dense flat color="primary" size="12px") форма 3-НДФЛ
+		.block
+			.hd Замещения
+			component(:is="SvgIcon" name="zamest" style="font-size:1.3rem;top: 8px; right: 5px" ).mon
+			.q-mt-sm
+				q-btn(dense flat color="primary" size="12px").q-mr-md Назначить заместителя
+				q-btn(dense flat color="primary" size="12px") Я - заместитель
+		.block.full
+			.hd Документы
+				q-badge(align="top") 3
+			component(:is="SvgIcon" name="docs" style="font-size:1.3rem;top: 8px" ).mon
 
 </template>
 
-<script setup lang="ts">
+<script setup>
 import { ref, reactive } from 'vue'
+import SvgIcon from '@/components/SvgIcon.vue'
 
-const search = ref()
+const money = ref(false)
+const toggleMoney = () => {
+	money.value = !money.value
+}
 </script>
 
 <style scoped lang="scss">
+@import '@/assets/styles/theme.scss';
+
+.text-overline {
+	line-height: 100%;
+}
+.mygrid {
+	margin-top: 1rem;
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	gap: 0.5rem;
+	.block {
+		// min-height: 100px;
+		background: #fff;
+		padding: 1rem;
+		position: relative;
+		border-radius: 4px;
+		border: 1px solid transparent;
+		cursor: pointer;
+		&.full {
+			grid-column: 1/-1;
+		}
+		&:hover {
+			border: 1px solid var(--q-primary);
+		}
+	}
+	.hd {
+		position: relative;
+		font-size: 1.2rem;
+		color: var(--text-color);
+		span {
+			margin-left: 2rem;
+			font-size: 0.8rem;
+			color: var(--q-link);
+		}
+	}
+}
+.tb {
+	margin-top: 1rem;
+	display: grid;
+	grid-template-columns: auto 1fr;
+	column-gap: 1rem;
+	transition: 0.2s ease all;
+	.q-separator {
+		grid-column: 1/-1;
+	}
+	&.blur {
+		filter: blur(7px);
+	}
+}
+@media screen and (max-width: 900px) {
+	.mygrid {
+		grid-template-columns: repeat(2, 1fr);
+	}
+}
+@media screen and (max-width: 680px) {
+	.mygrid {
+		grid-template-columns: repeat(1, 1fr);
+	}
+}
+
 .search {
 	width: 350px;
 	font-size: 1.1rem;
 	margin-bottom: 1rem;
-	:deep .q-field--prepend,
-	:deep .q-field--append {
+	:deep(.q-field--prepend),
+	:deep(.q-field--append) {
 		transform: translateY(5px);
 	}
 }
-:deep.q-field--dense .q-field__marginal {
-	height: inherit !important;
+.overline {
+	font-size: 1rem;
+	letter-spacing: 1px;
+}
+.mon {
+	position: absolute;
+	top: 0;
+	right: -5px;
+	font-size: 1.8rem;
+	fill: #888;
 }
 </style>
